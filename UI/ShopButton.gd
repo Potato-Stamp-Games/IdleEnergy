cc3d11cb22ffcacbc51b8077efdf682dc3f557be
ECrites <156072054+ECrites@users.noreply.github.com>
2024-02-13 17:35:58 -0600

    SHOP Scene
    Added shop button function
    added shop scene
    added back button function

--- UI/ShopButton.gd
+++ UI/ShopButton.gd
@@ -1,8 +1,3 @@
-extends Area2D
+extends TextureButton
 class_name ShopButton
 
-#When input is mouse, when mouse button left click, when pressed
-func _on_input_event(viewport, event, shape_idx):#on_input_event calls collisionshape2d signal
-	if  event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("Shop Button Clicked")
